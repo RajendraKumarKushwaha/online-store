2a5fe07f72165486168ed0bd9dcf9a54b659b125
RajendraKumarKushwaha <rajendra.cs909@gmail.com>
2025-02-27 10:26:30 +0530

    Update apiConfig.jsx

--- src/config/apiConfig.jsx
+++ src/config/apiConfig.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:5000"
+export const API_BASE_URL = "https://online-store-api-neon.vercel.app"
 
 const jwt = localStorage.getItem("jwt");
 
